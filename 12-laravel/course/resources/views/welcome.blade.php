76299ebb382c1eb30da8ad23fc6f97c47d79d784
mhjim <mhjim.info@gmail.com>
2025-07-14 17:59:53 +0600

    jul 14 final push

--- 12-laravel/course/resources/views/welcome.blade.php
+++ 12-laravel/course/resources/views/welcome.blade.php
@@ -1,8 +1,8 @@
-<x-slot:heading>
-    Home Page
-</x-slot:heading>
 
 <x-layout>
+    <x-slot:heading>
+        Home Page
+    </x-slot:heading>
     <h1>Home Page</h1>
     <p>Hello world~</p>
 </x-layout>
